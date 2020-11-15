a252ccb35ab4d4f9fd7fd730202ee746f1ef7319
Seokju Hong <iamseokjuhong@gmail.com>
2020-11-15 18:12:55 +0900

    2주차: 자판기 웹 디자인 구현, 모델 색상 랜덤 저장

--- vending/models.py
+++ vending/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.urls import reverse
+import random
+
+
+def set_hex_color():
+    r = lambda: random.randint(0, 255)
+    return '#%02X%02X%02X' % (r(), r(), r())
 
 
 class VendingMachine(models.Model):
@@ -21,9 +27,15 @@ class VendingItem(models.Model):
     name = models.CharField(max_length=30, verbose_name="상품명")
     quantity = models.PositiveIntegerField(default=0, verbose_name="수량")
     price = models.PositiveIntegerField(default=0, verbose_name="가격")
+    color = models.CharField(max_length=30, blank=True, verbose_name='색상')
 
     def __str__(self):
         return f"{self.name}"
+
+    def save(self, *args, **kwargs):
+        if not getattr(self, 'color'):
+            self.color = set_hex_color()
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = '상품'
